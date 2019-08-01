5cf7071cc1716dc7bbd358090910355283291f1a
Sean VanMeter <veritasium87@gmail.com>
2019-07-31 20:46:39 -0400

    Update docs

--- .travis/build.pl
+++ .travis/build.pl
@@ -17,6 +17,21 @@ $ENV{TRAVIS} = 1;
 $ENV{FORCE_CC} = $ENV{CC};
 $ENV{FORCE_CXX} = $ENV{CXX};
 
+if ($ENV{UNBRACE}) {
+    try("util/unbrace -n");
+    exit 0;
+}
+
+if ($ENV{CHECKWHITE}) {
+    try("util/checkwhite -an");
+    exit 0;
+}
+
+if ($ENV{CROSSCOMPILE}) {
+    try("make CROSSHOST=i686-w64-mingw32 package-windows");
+    exit 0;
+}
+
 if ($ENV{FULLDEBUG}) {
     try("make -j2 debug monster");
 }
